1e1abe267ffd61402f6a02105a5c61677abd285a
HamadKhushik <hammadkhan@live.com>
2022-04-17 23:33:43 +0100

    jdbc connection to mysql

---
+++ Type4DriverDemo.java
@@ -0,0 +1,17 @@
+import java.sql.*;
+
+public class Type4DriverDemo{
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("hello");
+        //Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/girrafe", "root", "London123");
+        Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery("select * from branch");
+
+        while (rs.next()){
+            System.out.println(rs.getInt(1) + "\t" + rs.getString(2) + "\t" + rs.getInt(3) + "\t" + rs.getDate(4));
+        }
+        con.close();
+    }
+}
