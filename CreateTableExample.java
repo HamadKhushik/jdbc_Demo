d9d0a8a3ade67e271121f411397dcad1c75eb0b4
HamadKhushik <hammadkhan@live.com>
2022-04-18 23:55:46 +0100

    creating table using jdbc

---
+++ CreateTableExample.java
@@ -0,0 +1,9 @@
+import sql.*;
+
+public class CreateTableExample{
+
+    public static void main(String[] args) {
+        String driver = "com.mysql.cj.jdbc.Driver";
+        String jdbc_url = "jdbc:mysql://localhost:3306/girrafe";
+    }
+}
