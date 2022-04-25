36118ff474b7ba2573560f077c2c68dccfc04897
HamadKhushik <hammadkhan@live.com>
2022-04-25 23:52:38 +0100

    get the employee details with the second highest salary - in progress

--- Aggregate_Functions/EmployeeWithNthHeighestSalary.java
+++ Aggregate_Functions/EmployeeWithNthHeighestSalary.java
@@ -16,7 +16,8 @@ public class EmployeeWithNthHeighestSalary {
         System.out.println("Please enter the required heighest salary");
         int n = Math.abs(sc.nextInt()); sc.nextLine();
         // count function -> returns the number of rows in table
-        String sql_query = String.format("select * from (select eno, ename, esal, eaddr, rank() over (order by esal desc)as 'first' ranking from employees)where ranking = %d", n);
+        String sql_query = String.format("select * from (select eno, ename, esal, eaddr, rank() over (order by esal desc) ranking from employees)where ranking = %d", n);
+        // working on rank() query on this db
         ResultSet rs = st.executeQuery(sql_query);
         while (rs.next()){  // if there are more than one emp with heighest sal
             System.out.printf("%-5d%-10s%-8.1f%-10s%n", rs.getInt(1), rs.getString(2), rs.getDouble(3), rs.getString(4));
